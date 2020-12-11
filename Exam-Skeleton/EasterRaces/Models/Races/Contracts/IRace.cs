e347dce9cda080fb7021249dbe046bf342724a23
zhulietayaneva <69650829+zhulietayaneva@users.noreply.github.com>
2020-12-11 02:20:19 +0200

    Add files via upload

---
+++ Exam-Skeleton/EasterRaces/Models/Races/Contracts/IRace.cs
@@ -0,0 +1,19 @@
+﻿using EasterRaces.Models.Drivers.Contracts;
+
+namespace EasterRaces.Models.Races.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+
+    public interface IRace
+    {
+        string Name { get; }
+
+        int Laps { get; }
+
+        IReadOnlyCollection<IDriver> Drivers { get; }
+
+        void AddDriver(IDriver driver);
+    
+    }
+}
---
+++ Exam-Skeleton/EasterRaces/Repositories/Entities/RaceRepository.cs
@@ -0,0 +1,41 @@
+﻿using EasterRaces.Models.Races.Contracts;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private readonly List<IRace> races;
+
+        public RaceRepository()
+        {
+            races = new List<IRace>();
+        }
+        public void Add(IRace model)
+        {
+            races.Add(model);
+        }
+
+        public IReadOnlyCollection<IRace> GetAll()
+        {
+            return races.ToArray();
+        }
+
+        public IRace GetByName(string name)
+        {
+            return races.FirstOrDefault(r => r.Name == name);
+        }
+
+        public bool Remove(IRace model)
+        {
+            return races.Remove(model);
+        }
+    }
+
+
+}
